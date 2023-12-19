d6a8aba4dadef60d71bcc3f9a05763b831e8edb4
ph0o0enix <153868627+ph0o0enix@users.noreply.github.com>
2023-12-19 17:52:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ph0o0enix/blackbot",
+    "https://github.com/ph0o0enix/quran.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
