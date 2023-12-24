3469e9c48a8bfcf6c6f4481c90b7aed47cb96de9
ph0o0enix <153868627+ph0o0enix@users.noreply.github.com>
2023-12-24 11:50:08 +0200

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -22,10 +22,8 @@ def private_panel(_):
         InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
         [
-        InlineKeyboardButton(
-                text= "₁ القناه ₁", url=f"https://t.me/S1_I_I"), 
-        InlineKeyboardButton(
-                text= "₂ القناه ₂", url=f"https://t.me/CH_KORAN")
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
